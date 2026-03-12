be44c3ac75a6858aea9153f9721f9297479383cd
Alex <darkimpulsepoint@gmail.com>
2026-03-12 14:00:25 +0300

    updated

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // base: "https://github.com/darkimpulsepoint/cv",
+  base: "/cv/",
+  build: {
+    outDir: "dist",
+  },
 });
